f17e7184c1fce60a32d633d146597398c080a724
taterntots <matthew.a.weidner@gmail.com>
2020-01-17 19:12:55 -0500

    got both my get and get by ID endpoints working.

--- server.js
+++ server.js
@@ -1,9 +1,63 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
-
 const server = express();
-
 server.use(express.json());
+
+server.get('/api/accounts', (req, res) => {
+  db
+    .select('*')
+    .from('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'Failed to get accounts' })
+    })
+})
+
+server.get('/api/accounts/:id', (req, res) => {
+  db
+    .select('*')
+    .from('accounts')
+    // .where('id', '=', req.params.id)
+    .where({ id: req.params.id })
+    .first() //grabs the first object in the array. Otherwise we will get an object in an array
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'Failed to get specified account' })
+    })
+})
+
+// shorthand for --- db.select('*').from('accounts')
+
+server.post('/api/accounts/:id', (req, res) => {
+  db('accounts') // shorthand for --- db.select('*').from('accounts')
+})
+
+// server.get('/api/accounts', async (req, res, next) => {
+//   try {
+//     //translates to 'SELECT * FROM accounts'
+//     res.json(await db.select('*').from('accounts'))
+//   } catch (err) {
+//       next(err)
+//   }
+// })
+
+// server.post('/api/accounts', async (req, res, next) => {
+//   try {
+//     const payload = {
+//       name: req.body.name,
+//       budget: req.body.budget
+//     }
+//     const [id] = await db('accounts').insert(payload)
+//     res.json({ id })
+//   } catch(error) {
+//       next(error)
+//   }
+// })
+
+
 
 module.exports = server;
